514e08b6adcd17d15d690162716748eebeee9116
alex <windev@nate.com>
2024-03-06 22:43:52 +0900

    D3DApp class implementatation

---
+++ D3DApp.cpp
@@ -0,0 +1,67 @@
+#include "D3DApp.h"
+
+D3DApp::D3DApp()
+{
+}
+
+D3DApp::~D3DApp()
+{
+}
+
+HRESULT D3DApp::InitD3D(HWND hWnd)
+{
+	m_hWnd = hWnd; 
+
+	if (NULL == (m_pD3D = Direct3DCreate9(D3D_SDK_VERSION)))
+		return E_FAIL; 
+
+	D3DPRESENT_PARAMETERS d3dpp; 
+	ZeroMemory(&d3dpp, sizeof(d3dpp)); 
+	d3dpp.Windowed = TRUE; 
+	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD; 
+	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN; 
+
+    if (FAILED(m_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd,
+        D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &m_pd3dDevice)))
+    {
+        return E_FAIL;
+    } 
+
+    OnInit(); 
+
+    return S_OK;
+}
+
+void D3DApp::Reder()
+{
+    if (NULL == m_pd3dDevice)
+        return;
+
+    m_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET, D3DCOLOR_XRGB(0, 100, 150), 1.0f, 0);
+
+    if (SUCCEEDED(m_pd3dDevice->BeginScene()))
+    {
+        OnRender(); 
+
+        m_pd3dDevice->EndScene();
+    }
+
+    m_pd3dDevice->Present(NULL, NULL, NULL, NULL);
+
+}
+
+void D3DApp::UpDate()
+{
+    OnUpdate(); 
+}
+
+void D3DApp::Cleanup()
+{
+    OnRelease(); 
+
+    if (m_pd3dDevice != NULL)
+        m_pd3dDevice->Release(); 
+
+    if (m_pD3D != NULL)
+        m_pD3D->Release(); 
+}
---
+++ D3DApp.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <d3d9.h>
+
+class D3DApp
+{
+protected:
+	LPDIRECT3D9			m_pD3D; 
+	LPDIRECT3DDEVICE9	m_pd3dDevice; 
+	HWND				m_hWnd; 
+
+protected:
+	virtual void OnInit() = 0; 
+	virtual void OnRender() = 0; 
+	virtual void OnUpdate() = 0; 
+	virtual void OnRelease() = 0; 
+
+public:
+	HRESULT InitD3D(HWND hWnd); 
+	void Reder(); 
+	void UpDate(); 
+	void Cleanup(); 
+
+public:
+	D3DApp(); 
+	~D3DApp(); 
+};
+
