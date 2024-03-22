d62cec57dd7923dae76b176e84242b6475d08aad
alex <windev@nate.com>
2024-03-23 01:20:33 +0900

    tree teapot rotation

--- Game1.cpp
+++ Game1.cpp
@@ -49,16 +49,36 @@ void Game1::OnUpdate()
 
 void Game1::OnRender()
 {
-	D3DXMATRIX matTranslation; 
+	D3DXMATRIX matScaling, matRotationY, matWorld, matTrans; 
+	float fScaling[3] = { 0.3f, 0.6f, 1.0f }; 
+	D3DXVECTOR3 vTrans[3] = { D3DXVECTOR3(0.0f, 0.0f, 0.0f), 
+		D3DXVECTOR3(2.0f, 0.0f, 0.0f), D3DXVECTOR3(5.0f, 0.0f, 0.0f) }; 
+	D3DXMatrixRotationY(&matRotationY, GetTickCount64() * 0.002f); 
 
 	m_Axis.OnRender(); 	
 
 	m_pd3dDevice->SetRenderState(D3DRS_LIGHTING, FALSE);
 	m_pd3dDevice->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);
 
-	D3DXMatrixTranslation(&matTranslation, 2.5f, 0.0f, 2.0f); 
-	m_pd3dDevice->SetTransform(D3DTS_WORLD, &matTranslation);
-	m_pTeapotMesh->DrawSubset(0); 	
+	D3DXMatrixRotationY(&matRotationY, GetTickCount64() * 0.002f);
+
+	D3DXMatrixScaling(&matScaling, fScaling[0], fScaling[0], fScaling[0]); 	
+	D3DXMatrixTranslation(&matTrans, vTrans[0].x, vTrans[0].y, vTrans[0].z); 
+	matWorld = matScaling * matTrans * matRotationY;
+	m_pd3dDevice->SetTransform(D3DTS_WORLD, &matWorld);
+	m_pTeapotMesh->DrawSubset(0); 
+
+	D3DXMatrixScaling(&matScaling, fScaling[1], fScaling[1], fScaling[1]);
+	D3DXMatrixTranslation(&matTrans, vTrans[1].x, vTrans[1].y, vTrans[1].z);
+	matWorld = matScaling * matTrans * matRotationY;
+	m_pd3dDevice->SetTransform(D3DTS_WORLD, &matWorld);
+	m_pTeapotMesh->DrawSubset(0);
+
+	D3DXMatrixScaling(&matScaling, fScaling[2], fScaling[2], fScaling[2]);
+	D3DXMatrixTranslation(&matTrans, vTrans[2].x, vTrans[2].y, vTrans[2].z);
+	matWorld = matScaling * matTrans * matRotationY;
+	m_pd3dDevice->SetTransform(D3DTS_WORLD, &matWorld);
+	m_pTeapotMesh->DrawSubset(0);
 }
 
 void Game1::OnRelease()
